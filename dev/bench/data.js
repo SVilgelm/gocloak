90e9bb6eb7dc56fb441a4e0924a95ed913480039
github-action-benchmark <github@users.noreply.github.com>
2019-12-29 04:56:20 +0000

    add Benchmark (go) benchmark result for f842af6833ae305ecbf936cc263c4a9181bf6d78

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1577594277013,
+  "lastUpdate": 1577595380428,
   "repoUrl": "https://github.com/SVilgelm/gocloak",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 65302146,
             "unit": "ns/op\t   66931 B/op\t     183 allocs/op",
             "extra": "21 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sergey.vilgelm@ibm.com",
+            "name": "Sergey Vilgelm",
+            "username": "SVilgelm"
+          },
+          "committer": {
+            "email": "sergey.vilgelm@ibm.com",
+            "name": "Sergey Vilgelm",
+            "username": "SVilgelm"
+          },
+          "distinct": true,
+          "id": "f842af6833ae305ecbf936cc263c4a9181bf6d78",
+          "message": "Move to github actions",
+          "timestamp": "2019-12-28T22:53:59-06:00",
+          "tree_id": "e5f66b250c0b8bf86c8aa3c30ac9649788616677",
+          "url": "https://github.com/SVilgelm/gocloak/commit/f842af6833ae305ecbf936cc263c4a9181bf6d78"
+        },
+        "date": 1577595380134,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLogin",
+            "value": 96134465,
+            "unit": "ns/op\t   53226 B/op\t     183 allocs/op",
+            "extra": "12 times"
+          },
+          {
+            "name": "BenchmarkLogin",
+            "value": 89567462,
+            "unit": "ns/op\t   59139 B/op\t     183 allocs/op",
+            "extra": "13 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoginParallel",
+            "value": 87942409,
+            "unit": "ns/op\t   46975 B/op\t     181 allocs/op",
+            "extra": "12 times"
+          },
+          {
+            "name": "BenchmarkLoginParallel",
+            "value": 53879731,
+            "unit": "ns/op\t   56563 B/op\t     183 allocs/op",
+            "extra": "19 times\n2 procs"
           }
         ]
       }
