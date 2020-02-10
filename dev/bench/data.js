de7293cc05e770bd05b949aa6fcd09b77e22118a
github-action-benchmark <github@users.noreply.github.com>
2020-02-10 20:37:19 +0000

    add Benchmark (go) benchmark result for f2db20af7716766d0f485fa2b1f4324c86134db2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1577595380428,
+  "lastUpdate": 1581367039731,
   "repoUrl": "https://github.com/SVilgelm/gocloak",
   "entries": {
     "Benchmark": [
@@ -95,6 +95,54 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkLoginParallel",
             "value": 53879731,
             "unit": "ns/op\t   56563 B/op\t     183 allocs/op",
+            "extra": "19 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sergey@vilgelm.info",
+            "name": "Sergey Vilgelm",
+            "username": "SVilgelm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2db20af7716766d0f485fa2b1f4324c86134db2",
+          "message": "Merge pull request #143 from dlisin/master\n\nAdding GetDefaultGroups, AddDefaultGroup, RemoveDefaultGroup methods",
+          "timestamp": "2020-02-10T14:34:01-06:00",
+          "tree_id": "3a87b3ca2dc496bece9393755e062af16d9592e7",
+          "url": "https://github.com/SVilgelm/gocloak/commit/f2db20af7716766d0f485fa2b1f4324c86134db2"
+        },
+        "date": 1581367039421,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLogin",
+            "value": 89615031,
+            "unit": "ns/op\t   70523 B/op\t     184 allocs/op",
+            "extra": "13 times"
+          },
+          {
+            "name": "BenchmarkLogin",
+            "value": 91932240,
+            "unit": "ns/op\t   45035 B/op\t     183 allocs/op",
+            "extra": "13 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoginParallel",
+            "value": 83945244,
+            "unit": "ns/op\t   58775 B/op\t     181 allocs/op",
+            "extra": "14 times"
+          },
+          {
+            "name": "BenchmarkLoginParallel",
+            "value": 67728072,
+            "unit": "ns/op\t   64364 B/op\t     184 allocs/op",
             "extra": "19 times\n2 procs"
           }
         ]
